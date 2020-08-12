7f182b161004f62cc7adfb943d1405509a8da8f4
Julia Kulla-Mader <julia.kullamader@microsoft.com>
2020-08-12 14:15:25 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+chmod +x {{ .Path }}; {{ .Vars }} sudo -E sh '{{ .Path }}'
+
+apt-get update
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+source ~/.profile
+nvm --version
+nvm install 10
+
+/usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
+
+/bin/sh -x
