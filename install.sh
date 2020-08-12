74991f221af8b5de8fefd20b3c2530bf71263821
Julia Kulla-Mader <julia.kullamader@microsoft.com>
2020-08-12 15:18:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 # chmod +x {{ .Path }}; {{ .Vars }} sudo -E sh '{{ .Path }}'
 
 apt-get update
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+nvm --version
+
 # source ~/.profile
 # nvm --version
 # nvm install 10
